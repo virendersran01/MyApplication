b1033854fc6ab350f456e8a97b3b419a695067d5
virendersran01 <virendersran01@gmail.com>
2019-12-03 22:31:41 +0530

    created custom view called Pizza .

--- app/src/main/java/com/virtualstudios/myapplication/customviews/Pizza.java
+++ app/src/main/java/com/virtualstudios/myapplication/customviews/Pizza.java
@@ -1,6 +1,7 @@
 package com.virtualstudios.myapplication.customviews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,12 +9,15 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 
 import com.virtualstudios.myapplication.R;
 
+
 public class Pizza extends View {
     private Paint paint;
+    private int mNumWedges = 5;
+    private int mColor = Color.YELLOW;
+    private int mStrokeWidth = 4;
 
     public Pizza(Context context) {
         super(context);
@@ -23,18 +27,29 @@ public class Pizza extends View {
     public Pizza(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
+        obtainAttributes(context, attrs);
     }
 
     public Pizza(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+        obtainAttributes(context, attrs);
+    }
+
+    private void obtainAttributes(Context context, @Nullable AttributeSet attrs){
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Pizza);
+        mNumWedges = typedArray.getInt(R.styleable.Pizza_color,mNumWedges);
+        mColor = typedArray.getColor(R.styleable.Pizza_color, mColor);
+        mStrokeWidth = typedArray.getDimensionPixelSize(R.styleable.Pizza_stroke_width, mStrokeWidth);
+        typedArray.recycle();
+
     }
 
     private void init(){
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(8);
-        paint.setColor(Color.YELLOW);
+        paint.setStrokeWidth(mStrokeWidth);
+        paint.setColor(mColor);
     }
 
     @Override
@@ -51,10 +66,9 @@ public class Pizza extends View {
     }
 
     private void drawPizzaCuts(Canvas canvas, float cx, float cy, float radius){
-        final int numWedges = 5;
-        final float degree = 360f / numWedges;
+        final float degree = 360f / mNumWedges;
         canvas.save();
-        for (int i=0;i<numWedges;i++) {
+        for (int i=0;i<mNumWedges;i++) {
             canvas.rotate(degree, cx, cy);
             canvas.drawLine(cx, cy, cx, cy - radius, paint);
         }
